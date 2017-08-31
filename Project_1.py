83506504b39f70c4f6e18c13ffe03094d40cadc9
Aingty Eung <aingtyeung@yahoo.com>
2017-08-31 13:29:59 -0700

    Added the Project 1 PDF to turn in.

--- Project_1.py
+++ Project_1.py
@@ -6,8 +6,6 @@
 # Pseudorandom Number Generator
 #   This program will generate a number based on user's input range.
 
-import time
-
 # The norm N is 10,000
 N = 10000
 
@@ -18,9 +16,8 @@ A = 4857
 M = 8601
 
 S = input("Enter seed number. ")
-#timeSeed = time.time()
 
 for i in range(100):
     S = (M*S + A)%N
-    R = S/float(N) # Float division is used to obtain the number on (0,1)
+    R = S/float(N) # Float division is used to obtain the number on (0,1) 
     print(format(R, '.4f')) # Print number to 4 decimal places
