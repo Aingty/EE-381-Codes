d9e69418e9f2b71bac39327de6b468c41892762e
Aingty Eung <aingtyeung@yahoo.com>
2017-09-14 09:44:00 -0700

    Finished project 2 part b

--- Project_2_Part_B.py
+++ Project_2_Part_B.py
@@ -9,21 +9,41 @@
 import math
 
 # The norm N is 10,000
-N = 10000
+N = 100000
 # The adder A is 4,857
 A = 4857
 # The multiplier is 8,601
 M = 8601
 
-Can = [0, 0, 0]
+Ball = [0, 0, 0]
 
-# Input the Seed
+# Initialize the counters
+sum_One = 0
+sum_Two = 0
+
+# Initialize the Seed
 #S = input("Enter seed number. ")
-S = 0;
+S = 0
 
-for i in range(2):
-    S = (M*S + A)%N
-    R = S/float(N) # Float division is used to obtain the number on (0,1) 
-    Can_Number = math.floor(R*5 + 1)
-    Can[i] = Can_Number
-    print(Can)
+# Initialize the number of Experiments
+k = int(input('Enter the number of experiments. '))
+
+for k in range(k): 
+    for i in range(3):
+        S = (M*S + A)%N
+        R = S/float(N) # Float division is used to obtain the number on (0,1) 
+
+        # Generating number 1 - 5
+        Ball_Number = math.floor(R*5 + 1)
+
+        # Saving the randomized number into list
+        Ball[i] = int(Ball_Number)
+
+        if (i == 2) & ((Ball[0] != Ball[1]) & (Ball[1] != Ball[2]) & (Ball[0] != Ball[2])):
+            sum_One += 1
+            #print('Distinct', Ball)
+        elif i == 2:
+            sum_Two += 1
+            #print('Not Distinct', Ball)
+
+print(sum_One / float((sum_One + sum_Two)))
