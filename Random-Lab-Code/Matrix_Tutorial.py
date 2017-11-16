e9c8ec6521aa17af85c6cbf02bf56fd7ea591053
Aingty Eung <aingtyeung@yahoo.com>
2017-11-16 10:11:45 -0800

    Worked on Matrix Eigen Installed numpy

--- Random-Lab-Code/Matrix_Tutorial.py
+++ Random-Lab-Code/Matrix_Tutorial.py
@@ -3,4 +3,11 @@ import numpy as np
 a = np.matrix('2 4 ; 1 5')
 b = np.matrix('1 4 ; -2 3')
 
-print(a*b)
+print(a*b, '\n')
+
+c = np.matrix('3 2 ; -1 0')
+
+w, v = np.linalg.eig(c)
+
+print('Lambda: \n', w)
+print('\nVector: \n', v)
